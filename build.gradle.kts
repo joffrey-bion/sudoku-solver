69670db58fe353e35c77ffb1b96b8fa8fba5d3b9
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-08 21:59:42 +0100

    Update build.gradle and switch to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+group = "org.hildan.sudoku"
+
+application {
+    mainClass.set("org.hildan.sudoku.Sudoku")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
