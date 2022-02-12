f720c1f9677237b5acecc0db45e87b89e929363b
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-12 20:07:31 +0100

    Add quick test on personal grids

--- src/test/kotlin/org/hildan/sudoku/test/SolverTest.kt
+++ src/test/kotlin/org/hildan/sudoku/test/SolverTest.kt
@@ -10,26 +10,48 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.nio.file.Path
 import kotlin.io.path.Path
-import kotlin.io.path.forEachLine
 import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.useLines
+import kotlin.system.measureTimeMillis
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.milliseconds
 
 @Execution(ExecutionMode.CONCURRENT)
 class SolverTest {
 
     @ParameterizedTest
+    @MethodSource("personalPuzzles")
+    fun testPersonalPuzzles(puzzlesFile: Path) {
+        testAllPuzzlesIn(puzzlesFile)
+    }
+
+    @ParameterizedTest
     @MethodSource("standardPuzzles")
-    fun testAllPuzzles(puzzlesFile: Path) {
-        puzzlesFile.forEachLine { encodedGrid ->
-            val grid = Grid(encodedGrid)
-            grid.solveWithBacktracking()
-            assertEquals(CheckResult.Valid, grid.check())
+    fun testStandardPuzzles(puzzlesFile: Path) {
+        testAllPuzzlesIn(puzzlesFile)
+    }
+
+    private fun testAllPuzzlesIn(puzzlesFile: Path) {
+        val avgTime = puzzlesFile.useLines { grids ->
+            grids.map { encodedGrid ->
+                measureTimeMillis { solveAndCheck(encodedGrid) }
+            }.average().milliseconds
         }
+        println("Average time $avgTime/grid for $puzzlesFile")
+    }
+
+    private fun solveAndCheck(encodedGrid: String) {
+        val grid = Grid(encodedGrid)
+        grid.solveWithBacktracking()
+        assertEquals(CheckResult.Valid, grid.check())
     }
 
     companion object {
 
         @JvmStatic
-        fun standardPuzzles(): List<Path> = Path("dataset/standard").listDirectoryEntries()
+        fun personalPuzzles(): List<Path> = Path("dataset/personal").listDirectoryEntries()
+
+        @JvmStatic
+        fun standardPuzzles(): List<Path> = Path("dataset/standard").listDirectoryEntries("puzzles*.txt")
     }
 }
