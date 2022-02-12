19526a658cbdfe89762fe70d6183e3f5aea20e2d
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-12 03:45:46 +0100

    Cleanup grid structure

--- src/main/kotlin/org/hildan/sudoku/model/Grid.kt
+++ src/main/kotlin/org/hildan/sudoku/model/Grid.kt
@@ -9,80 +9,57 @@ import org.hildan.sudoku.drawing.format
  * @throws IllegalArgumentException If there is not enough numbers, or too many, or some other characters than numbers.
  */
 class Grid(numbers: String) {
-    /**
-     * The matrix of the tiles of this `Grid`.
-     */
-    private val tiles: Array<Array<Tile>>
+
+    init {
+        require(numbers.length == SIZE * SIZE) {
+            "there must be ${SIZE * SIZE} input digits, got ${numbers.length}"
+        }
+    }
 
     /**
      * The list of the empty tiles of this `Grid`.
      */
-    val emptyTiles: MutableList<Tile>
+    val emptyTiles: MutableList<Tile> = ArrayList()
 
     /**
      * Whether this `Grid` is full of digits.
      */
-    val isFull: Boolean
+    val isComplete: Boolean
         get() = emptyTiles.isEmpty()
 
+    val tiles: List<Tile> = numbers.mapIndexed { index, c ->
+        when (c) {
+            in '1'..'9' -> Tile(this, index / SIZE, index % SIZE, c.digitToInt())
+            '.', ' ', '0' -> Tile(this, index / SIZE, index % SIZE).also { emptyTiles.add(it) }
+            else -> throw IllegalArgumentException("wrong input, only digits from 0 to $SIZE are accepted")
+        }
+    }
+
+    val rows: List<List<Tile>> = tiles.chunked(SIZE)
+    val cols: List<List<Tile>> = List(SIZE) { col -> rows.map { it[col] } }
+    val boxes: List<List<Tile>> = List(SIZE) { index ->
+        val rowOffset = (index / 3) * BOX_SIZE
+        val colOffset = (index % 3) * BOX_SIZE
+        buildList {
+            for (r in 0 until BOX_SIZE) {
+                for (c in 0 until BOX_SIZE) {
+                    add(get(rowOffset + r, colOffset + c))
+                }
+            }
+        }
+    }
+
     init {
-        require(numbers.length >= SIZE * SIZE) { "too few input digits (blanks must be given by zeros)" }
-        require(numbers.length <= SIZE * SIZE) { "too many input digits, only " + SIZE * SIZE + " are needed" }
-
-        emptyTiles = ArrayList()
-        tiles = Array(SIZE) { i ->
-            Array(SIZE) { j ->
-                when (val value = numbers[SIZE * i + j]) {
-                    in '1'..'9' -> Tile(this, i, j, value.digitToInt())
-                    '.', ' ', '0' -> Tile(this, i, j).also { emptyTiles.add(it) }
-                    else -> throw IllegalArgumentException("wrong input, only digits from 0 to $SIZE are accepted")
-                }
-            }
-        }
-        for (i in 0 until SIZE) {
-            for (j in 0 until SIZE) {
-                tiles[i][j].sisters = getSisters(i, j)
+        tiles.forEach {
+            it.sisters = buildSet {
+                addAll(rows[it.row])
+                addAll(cols[it.col])
+                addAll(boxes[boxIndex(it.row, it.col)])
             }
         }
     }
 
-    operator fun get(row: Int, col: Int) = tiles[row][col]
-
-    /**
-     * Returns the tiles which are either in the same row or column or region as the specified coordinates.
-     */
-    private fun getSisters(row: Int, col: Int): Set<Tile> {
-        val sisters = HashSet<Tile>()
-        // add the row
-        for (j in 0 until SIZE) {
-            if (j == col) {
-                continue
-            }
-            sisters.add(tiles[row][j])
-        }
-        // add the column
-        for (i in 0 until SIZE) {
-            if (i == row) {
-                continue
-            }
-            sisters.add(tiles[i][col])
-        }
-        // add the region
-        val baserow = row / BOX_SIZE * BOX_SIZE
-        val basecol = col / BOX_SIZE * BOX_SIZE
-        for (i in 0 until BOX_SIZE) {
-            if (baserow + i == row) {
-                continue  // skips the row, already added
-            }
-            for (j in 0 until BOX_SIZE) {
-                if (basecol + j == col) {
-                    continue  // skips the column, already added
-                }
-                sisters.add(tiles[baserow + i][basecol + j])
-            }
-        }
-        return sisters
-    }
+    operator fun get(row: Int, col: Int) = tiles[row * SIZE + col]
 
     /**
      * Iterates on complete [Tile]s and remove the corresponding value in the empty sister `Tile`s.
@@ -90,14 +67,12 @@ class Grid(numbers: String) {
      * @return `false` if an empty [Tile] ends up with no possible value.
      */
     fun clearImpossibleValues(): Boolean {
-        for (row in tiles) {
-            for (tile in row) {
-                if (tile.isEmpty) {
-                    continue  // do not consider empty tiles
-                }
-                if (!tile.removeValueFromSisters()) {
-                    return false
-                }
+        for (tile in tiles) {
+            if (tile.isEmpty) {
+                continue  // do not consider empty tiles
+            }
+            if (!tile.removeValueFromSisters()) {
+                return false
             }
         }
         return true
@@ -120,3 +95,5 @@ class Grid(numbers: String) {
         const val SIZE = BOX_SIZE * BOX_SIZE
     }
 }
+
+private fun boxIndex(row: Int, col: Int): Int = (row / Grid.BOX_SIZE) * 3 + col / Grid.BOX_SIZE
