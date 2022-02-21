d0ce9f1c5240f478170564946b7a1093813c0230
Joffrey Bion <joffrey.bion@gmail.com>
2022-02-21 21:32:42 +0100

    Add ignored tests for future techniques

--- solver/src/commonTest/kotlin/org/hildan/sudoku/solver/HumanSolverTest.kt
+++ solver/src/commonTest/kotlin/org/hildan/sudoku/solver/HumanSolverTest.kt
@@ -4,6 +4,7 @@ import org.hildan.sudoku.checker.CheckResult
 import org.hildan.sudoku.checker.check
 import org.hildan.sudoku.drawing.format
 import org.hildan.sudoku.model.Grid
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -40,6 +41,36 @@ class HumanSolverTest {
     // requires Jellyfish, Naked Triple, Pointing Pairs
     @Test
     fun jellyfish() = test("020000030400000007001230400004150300005640100000000000002510600500000090080000005")
+
+    // requires Naked Pairs, Simple Coloring on 2s
+    @Ignore
+    @Test
+    fun simpleColoring() = test("179850000632497851854000709293500467581746392467329518706905100918000005305100900")
+
+    // requires Naked Tuples, X-Wing and Y-Wing
+    @Ignore
+    @Test
+    fun wiki_tough() = test("309000400200709000087000000750060230600904008028050041000000590000106007006000104")
+
+    // requires X-Wing, Y-Wing, Simple Coloring, Naked Triples
+    @Ignore
+    @Test
+    fun wiki_xWing() = test("093004560060003140004608309981345000347286951652070483406002890000400010029800034")
+
+    // requires Pointing pairs, Swordfish, XY-Chain
+    @Ignore
+    @Test
+    fun wiki_swordfish() = test("050030602642895317037020800023504700406000520571962483214000900760109234300240170")
+
+    // requires Naked Triples, Box/Line Reduction, Jellyfish, Y-Wing
+    @Ignore
+    @Test
+    fun wiki_jellyfish() = test("024090008800402900719000240075804300240900587038507604082000059007209003490050000")
+
+    // requires WXYZ Wing, Alternating Inference Chains, Swordfish, Simple Coloring, XY-Chain
+    @Ignore
+    @Test
+    fun wiki_emptyRectangle() = test("000602400002403600000080021070000840309040207045000030500060000003159780008204500")
 
     private fun test(encodedGrid: String) {
         val grid = Grid(encodedGrid)
